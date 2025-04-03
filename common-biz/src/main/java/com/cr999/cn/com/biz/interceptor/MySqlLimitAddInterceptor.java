9e3876936f513d33df037724fdeb66ae0b94785d
wq <wq>
2025-04-03 15:28:52 +0800

    where条件默认添加deleted=0条件

--- common-biz/src/main/java/com/cr999/cn/com/biz/interceptor/MySqlLimitAddInterceptor.java
+++ common-biz/src/main/java/com/cr999/cn/com/biz/interceptor/MySqlLimitAddInterceptor.java
@@ -1,36 +1,17 @@
 package com.cr999.cn.com.biz.interceptor;
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.baomidou.mybatisplus.core.override.PageMapperMethod;
-import com.baomidou.mybatisplus.core.toolkit.ObjectUtils;
 import com.cr999.cn.com.biz.service.SystemParameterService;
-import com.cr999.cn.common.ConstantEnum;
-import org.apache.ibatis.binding.MapperMethod;
-import org.apache.ibatis.executor.Executor;
 import org.apache.ibatis.executor.statement.StatementHandler;
 import org.apache.ibatis.mapping.BoundSql;
-import org.apache.ibatis.mapping.MappedStatement;
-import org.apache.ibatis.mapping.ParameterMapping;
 import org.apache.ibatis.plugin.*;
-import org.apache.ibatis.reflection.MetaObject;
-import org.apache.ibatis.session.Configuration;
-import org.apache.ibatis.session.ResultHandler;
-import org.apache.ibatis.session.RowBounds;
-import org.apache.ibatis.type.TypeHandlerRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 import java.lang.reflect.Field;
 import java.sql.Connection;
-import java.sql.Statement;
-import java.text.DateFormat;
-import java.util.*;
-import java.util.regex.Matcher;
+import java.util.Properties;
 
 /**
  * <p>
@@ -54,10 +35,8 @@ import java.util.regex.Matcher;
 @Intercepts({
 		@Signature(type =StatementHandler.class, method = "prepare", args = {Connection.class, Integer.class})
 })
-//@Component
 public class MySqlLimitAddInterceptor implements Interceptor {
 
-	//@Value("${limit.count}")
 	public static long limitCount=0;
 
 	@Resource
@@ -77,66 +56,34 @@ public class MySqlLimitAddInterceptor implements Interceptor {
 		StatementHandler statementHandler=(StatementHandler)invocation.getTarget();
 		BoundSql boundSql = statementHandler.getBoundSql();
 		String originalSql = boundSql.getSql();
-		//String sql=originalSql.toLowerCase();
-		String modifiedSql=originalSql.toLowerCase();;
+		originalSql=originalSql.toLowerCase();
+		StringBuffer sqlBuffer=new StringBuffer(originalSql);
+
 		//自动增加条件where deleted=0，
-		if(modifiedSql.startsWith("select") || modifiedSql.startsWith("update")){
-			String whereSuffix=modifiedSql.substring(modifiedSql.lastIndexOf("where"));
+		if(originalSql.startsWith("select") || originalSql.startsWith("update")){
+			String whereSuffix=originalSql.substring(originalSql.lastIndexOf("where"));
 			if(whereSuffix.contains("where")){
 				if(!whereSuffix.contains("deleted=")){
-					modifiedSql=modifiedSql+" and deleted=0";
+					sqlBuffer.append(" and deleted=0");
 				}
 			}else{
-				modifiedSql=modifiedSql+" where deleted=0";
+				sqlBuffer.append(" where deleted=0");
 			}
 		}
 
-
-
-		if (modifiedSql.startsWith("select") && !modifiedSql.contains("limit") && !modifiedSql.contains("rownum")) {
+		if (originalSql.startsWith("select") && !originalSql.contains("limit") && !originalSql.contains("rownum")) {
 			SystemParameterService systemParameterService= (SystemParameterService) configurableApplicationContext.getBean("systemParameterServiceImpl");
 			String limit_count = systemParameterService.getParmValue("limit_count", "*");
 			limitCount=Integer.valueOf(limit_count);
-			modifiedSql=modifiedSql+" limit "+ limitCount;
+			sqlBuffer.append(" limit ").append(limitCount);
 
 			Field field = boundSql.getClass().getDeclaredField("sql");
 			field.setAccessible(true);
-			field.set(boundSql,modifiedSql);
+			field.set(boundSql,sqlBuffer.toString());
 		}
 		return invocation.proceed();
 	}
-	public void aa(Invocation invocation){
-		StatementHandler statementHandler=(StatementHandler)invocation.getTarget();
-		BoundSql boundSql = statementHandler.getBoundSql();
-		String originalSql = boundSql.getSql();
 
-		MapperMethod.ParamMap parameterObject = (MapperMethod.ParamMap) boundSql.getParameterObject();
-		QueryWrapper queryWrapper = (QueryWrapper) parameterObject.get("param1");
-		Map paramNameValuePairsMap = queryWrapper.getParamNameValuePairs();
-		List<ParameterMapping> params = boundSql.getParameterMappings();
-		for(ParameterMapping param:params){
-			String property = param.getProperty();
-			property=property.substring(property.lastIndexOf(".")+1);
-			Object obj = paramNameValuePairsMap.get(property);
-			originalSql = originalSql.replaceFirst("\\?", Matcher.quoteReplacement(getParameterValue(obj)));
-		}
-
-		System.out.println(originalSql);
-	}
-
-
-	private static String getParameterValue(Object object) {
-		String value = "";
-		if (object instanceof String){
-			value = "'" + object.toString() + "'";
-		}else if (object instanceof Date){
-			DateFormat format = DateFormat.getDateTimeInstance(DateFormat.DEFAULT, DateFormat.DEFAULT, Locale.CHINA);
-			value = "'" + format.format((Date) object) + "'";
-		} else if (!ObjectUtils.isEmpty(object)) {
-			value = object.toString();
-		}
-		return value;
-	}
 	@Override
 	public Object plugin(Object target) {
 		return Plugin.wrap(target, this);
